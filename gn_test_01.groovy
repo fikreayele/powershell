0e1e665bff23183b35dd6e1a9943a7a9a075f3ce
fikreayele <88599695+fikreayele@users.noreply.github.com>
2023-06-21 20:36:45 -0400

    Create gn_test_01.groovy

---
+++ gn_test_01.groovy
@@ -0,0 +1,16 @@
+node {
+    stage('Build') {
+        echo 'Building...'
+        // Add your build steps here
+    }
+
+    stage('Test') {
+        echo 'Testing...'
+        // Add your testing steps here
+    }
+
+    stage('Deploy') {
+        echo 'Deploying...'
+        // Add your deployment steps here
+    }
+}
